4022fb413ad7372fa99a9547989c43117cbc7207
antblessing <antblessing@gmail.com>
2016-03-19 12:26:40 -0700

    playsong and documentation

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,7 +1,10 @@
 (function() {
   function SongPlayer() {
     var SongPlayer = {};
-
+    /**
+    * @desc Song item 
+    * @type {Object}
+    */
     var currentSong = null;
     /**
     * @desc Buzz object audio file
@@ -26,12 +29,20 @@
 
       currentSong = song;
     };
+    /**
+    * @function playSong
+    * @desc Plays the currentBuzzObject and sets the song.playing boolean to true
+    * @param {Object} song
+    */
+    var playSong = function(song) {
+      currentBuzzObject.play();
+      song.playing = true;
+    };
 
     SongPlayer.play = function(song) {
       if (currentSong !== song) {
-        setSong(song)
-        song.playing = true;
-        currentBuzzObject.play();
+        setSong(song);
+        playSong(song);
       } else if (currentSong === song) {
           if (currentBuzzObject.isPaused()) {
             currentBuzzObject.play();
